a0b6b70ec456fbb16a21ddefdf6a0536ffce16d1
Lautaro Lezcano <lautarolezcano@gmail.com>
2025-02-06 08:24:47 -0300

    da'car

---
+++ imagenes/function.js
@@ -0,0 +1,17 @@
+//Escribe un programa en el lenguaje que prefieras que:
+
+Solicite al usuario que ingrese una serie de números separados por comas.
+Ejemplo: 5, 12, 8, 3, 22
+
+Convierta esa entrada en un array o lista de números.
+
+Encuentre y muestre el número más grande del array.
+
+Bonus (opcional):
+Verifica que la entrada del usuario sea válida (por ejemplo, que todos los valores sean números).
+Si el array está vacío, muestra un mensaje indicando que no hay números para analizar.
+
+let number = prompt ("ingresá hasta 4 numeros separados por coma")
+number = parseInt (number);
+console.log (number)
+)
